20ff5fff765cc5e7f5a5967691aff3c950340897
nitish ranjan <36075994+bestpain@users.noreply.github.com>
2018-04-12 11:42:54 +0530

    Add files via upload

---
+++ ques2.c
@@ -0,0 +1,35 @@
+// C program to create a simple linked list with 3 nodes,nodes are added at the beginning.
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+	int data;
+	struct node* next;
+};
+struct node* createnode(int data)
+{
+	struct node * temp=(struct node*)malloc(sizeof(struct node));
+	temp->data=data	;
+	temp->next=NULL;
+	return temp;
+}
+
+void printlist(struct node* head)
+{
+		while(head!=NULL)
+		{
+			printf("%d  ",head->data );
+			head=head->next;
+		}
+}
+int main(){
+//step 1:we created three independent nodes and then we stored the address each nodes in a pointer to node data type.
+struct node* node1=createnode(5);
+struct node* node2=createnode(10);
+struct node* node3=createnode(15);
+//step 2:we have linked all the nodes together,new nodes are added at beginning.
+node3->next=node2;
+node2->next=node1;
+//step3:printing the list by passing the address of first node.
+printlist(node3);
+}
