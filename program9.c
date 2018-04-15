db871307ef07415620ef257ffc84810fc9c771ba
nitish ranjan <36075994+bestpain@users.noreply.github.com>
2018-04-15 18:30:22 +0530

    Create program9.c

---
+++ program9.c
@@ -0,0 +1,59 @@
+//deleteing a node by value in a linked list.
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{	
+	int data;
+	struct node* next;
+};
+struct node* head;
+void createnodeatbeg(int key)
+{
+		struct node*new=(struct node*)malloc(sizeof(struct node));
+		new->data=key;
+		new->next=head;
+		head=new;
+}
+
+int length()
+{
+	int count=0;
+	struct node* temp=head;
+	while(temp!=NULL)
+		{
+			count++;
+			temp=temp->next;
+		}
+	return count;
+
+}
+void printlist()
+{
+		struct node* temp=head;
+		printf("list is:");
+		while(temp!=NULL)
+		{
+			printf("%d  ",temp->data );
+			temp=temp->next;
+		}
+		printf("\n");
+}
+
+int main()
+{	
+	head=NULL;
+	int nodes,key;
+	printf("enter number of nodes\n");
+	scanf("%d",&nodes);
+	for(int i=0;i<nodes;i++)
+	{
+	int data;
+	printf("enter number\n");
+	scanf("%d",&data);
+	createnodeatbeg(data);
+	}
+	printlist();
+	int size=length();
+	printf("the number of nodes is %d\n",size);
+	
+}
